73821e0dfe327cf7b7b25831d9374c3b9993e88c
Nick Pilon <npilon@oreilly.com>
2008-08-01 11:14:56 -0700

    friendly_curl requires PyCURL. Who knew?

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(name='friendly_curl',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+	"pycurl>=7.16.4",
           # -*- Extra requirements: -*-
       ],
       entry_points="""
