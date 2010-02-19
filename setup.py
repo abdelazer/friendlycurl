31e414ad75a1b3aaceddeb8b7576ec135cdddd37
Gavin Carothers <gavin@oreilly.com>
2010-02-19 15:12:45 -0800

    Preparing to release on pypi

--- setup.py
+++ setup.py
@@ -8,17 +8,24 @@ setup(name='friendly_curl',
       description="A friendly interface to PyCURL.",
       long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='curl url http',
       author='Nick Pilon, Gavin Carothers',
       author_email='npilon@oreilly.com, gavin@oreilly.com',
-      url='',
+      url='http://code.google.com/p/friendlycurl/',
       license='Apache 2',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-	"pycurl>=7.16.4", "httplib2",
+	    "pycurl>=7.16.4", "httplib2",
           # -*- Extra requirements: -*-
       ],
       entry_points="""
