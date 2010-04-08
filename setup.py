158e274749f283922e4f67778cc284d973ae2e06
Nick Pilon <npilon@oreilly.com>
2010-04-08 11:31:29 -0700

    Development of 0.9.1

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9.1'
+
+setup(name='friendly_curl',
+      version=version,
+      description="A friendly interface to PyCURL.",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Nick Pilon, Gavin Carothers',
+      author_email='npilon@oreilly.com, gavin@oreilly.com',
+      url='',
+      license='Apache 2',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+	"pycurl>=7.16.4", "httplib2",
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
